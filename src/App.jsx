cc37d95dd65fc41c6e9dc9674e8399bc4f443afb
ioangheraszim <gijohny17@gmail.com>
2023-10-15 10:26:32 +0100

    fixed issue with navbar beind under the carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,24 @@
-import React from "react";
+
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFilm, faHouse, faMagnifyingGlass, faTv, faBars} from '@fortawesome/free-solid-svg-icons'
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import Navbar from "./components/Navbar";
 
 
 function App() {
+  
   return (
-    <main className="text-primary bg-background">
+    <main className="text-primary  -z-20">
       {/* Navbar */}
-      <header className="bg-opacity-80 backdrop-blur-lg bg-accent font-semibold">
-      <nav className="p-4">
-        <div className="container transition-all duration-300 mx-auto flex items-center justify-between">
-          <a href="#" className="text-3xl font-bold text-white">Ani<span className="text-cool">Net</span></a>
-          <ul class="hidden md:flex space-x-4 md:items-center md:justify-center md:flex-grow">
-            <li><a href="#" className="hover:bg-cool bg-opacity-40 p-5 rounded"><FontAwesomeIcon icon={faHouse} /></a></li>
-            <li><a href="#" className="hover:bg-cool bg-opacity-40 p-5 rounded"><FontAwesomeIcon icon={faTv} /></a></li>
-            <li><a href="#" className="hover:bg-cool bg-opacity-40 p-5 rounded"><FontAwesomeIcon icon={faFilm} /></a></li>
-            <li><a href="#" className="hover:bg-cool bg-opacity-40 p-5 rounded"><FontAwesomeIcon icon={faEnvelope} /></a></li>
-          </ul>
-          <div class="hidden md:flex space-x-4">
-            <button className="bg-white bg-opacity-40 rounded p-2"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
-          </div>
-          <div class="md:hidden flex items-center space-x-4">
-            <button className="bg-white bg-opacity-40 rounded p-2"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
-            <button className="bg-white bg-opacity-40 rounded p-2"><FontAwesomeIcon icon={faBars} /></button>
-          </div>
-        </div>
-      </nav>
-      </header>
+     <Navbar/>
       {/* Hero Section */}
-      <section className="container transition-all duration-300 mx-auto flex items-center justify-between">
-       <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false} showStatus={false}>
+      <section className="container mx-auto flex justify-center -z-10 p-5">
+      <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false} showStatus={false}>
         <div className="carousel-image">
             <img className="w-full" src="https://images6.alphacoders.com/785/thumb-1920-785425.png" alt="image" />
             <p className="carousel-text">Fate Stay Night</p>
         </div>
         <div className="carousel-image">
-            <img src="https://wallpapers.com/images/hd/demon-slayer-4k-pictures-5v5lz47uso2tx2kr.jpg" alt="image" />
+            <img src="https://assets1.ignimgs.com/thumbs/userUploaded/2021/10/12/demonslayerkimetsunoyaibathehinokamichroniclesblogroll-1634105238950.jpg" alt="image" />
             <p className="carousel-text">Demon Slayer</p>
         </div>
         <div className="carousel-image">
@@ -46,7 +26,9 @@ function App() {
             <p className="carousel-text">My Hero Academi</p>
         </div>
        </Carousel>
+
       </section>  
+
       {/* Trending */}
 
       {/* Popular */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilm, faHouse, faMagnifyingGlass, faTv, faBars, faXmark} from '@fortawesome/free-solid-svg-icons'
 
 function Navbar() {
+  const [toggleNav, setToggleNav] = useState(false);
+
+  const handleNav = () => {
+    setToggleNav(!toggleNav)
+  }
+
   return (
-    <div>Navbar</div>
+    <header className="bg-opacity-80 backdrop-blur-lg bg-accent font-semibold z-10 relative">
+    <nav className="p-4">
+      <div className="container transition-all duration-300 mx-auto flex items-center justify-between">
+        <a href="#" className="text-3xl font-bold text-white">Ani<span className="text-cool">Net</span></a>
+        <ul className={`${toggleNav ? "flex flex-col absolute top-0 right-0 rounded-br-lg rounded-bl-lg justify-around items-center h-96 bg-opacity-80 backdrop-blur-lg bg-accent font-semibold md:hidden transition-all duration-300 w-20 z-10" : "overflow-hidden h-0 p-0 m-0 z-10"} md:h-14 md:flex md:transition-all md:duration-300 space-x-4 md:items-center md:justify-center md:flex-grow`} style={{ zIndex: 1090 }}>
+          <li><button className={`${toggleNav ? "" : "hidden"} hover:bg-cool px-4 rounded`} onClick={handleNav}><FontAwesomeIcon icon={faXmark} /></button></li>
+          <li><a href="#" className="hover:bg-cool bg-opacity-40 p-4 mr-3 rounded"><FontAwesomeIcon icon={faHouse} /></a></li>
+          <li><a href="#" className="hover:bg-cool bg-opacity-40 p-4 mr-3 rounded"><FontAwesomeIcon icon={faTv} /></a></li>
+          <li><a href="#" className="hover:bg-cool bg-opacity-40 p-4 mr-3 rounded"><FontAwesomeIcon icon={faFilm} /></a></li>
+        </ul>
+        <div className="hidden md:flex space-x-4">
+          <button className="bg-white bg-opacity-40 rounded p-2"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+        </div>
+        
+        <div className="md:hidden flex items-center space-x-4">
+          <button className="bg-white bg-opacity-40 rounded p-2 mr-7"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+          <button onClick={handleNav} className="bg-white bg-opacity-40 rounded p-2">{toggleNav ? <></> : <FontAwesomeIcon icon={faBars} /> }</button>
+        </div>
+      </div>
+    </nav>
+
+    
+    </header>
   )
 }
 
